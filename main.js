c548c7ff5db7eb6fb82b03edb64898a1a80c6817
DocCodes <evanyoung62@icloud.com>
2018-01-02 11:45:42 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+const {app, BrowserWindow, dialog, shell} = require('electron')
+const fs = require('fs')
+const path = require('path')
+const url = require('url')
+let win
+
+
+try {
+  global.__settings = JSON.parse(fs.readFileSync(path.join(app.getPath('userData'), 'settings.json')))
+} catch (e) {
+  if (e.code === 'ENOENT') {
+    let defaultSettings = {
+      "home": "home"
+    }
+    fs.writeFileSync(path.join(app.getPath('userData'), 'settings.json'), JSON.stringify(defaultSettings))
+    global.__settings = JSON.parse(fs.readFileSync(path.join(app.getPath('userData'), 'settings.json')))
+  } else {throw e}
+}
+try {
+  global.subsites = fs.readdirSync(path.join(app.getPath('userData'), 'sites'))
+} catch (e) {
+  if (e.code === 'ENOENT') {
+    fs.mkdirSync(path.join(app.getPath('userData'), 'sites'))
+    global.subsites = fs.readdirSync(path.join(app.getPath('userData'), 'sites'))
+  } else {throw e}
+}
+
+
+function createWindow () {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 600,
+    minHeight: 600,
+    backgroundColor: '#181818'
+  })
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, `index.html`),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
---
+++ scripts/builder.js
@@ -0,0 +1,186 @@
+const remote = require('electron').remote
+const fs = require('fs')
+const path = require('path')
+const url = require('url')
+/*
+██████  ██████   ██████  ████████  ██████  ████████ ██    ██ ██████  ███████
+██   ██ ██   ██ ██    ██    ██    ██    ██    ██     ██  ██  ██   ██ ██
+██████  ██████  ██    ██    ██    ██    ██    ██      ████   ██████  █████
+██      ██   ██ ██    ██    ██    ██    ██    ██       ██    ██      ██
+██      ██   ██  ██████     ██     ██████     ██       ██    ██      ███████
+*/
+// <region> Prototype
+Location.prototype.query = (() => {
+  let qs = document.location.search.split('+').join(' ')
+  let re = /[?&]?([^=]+)=([^&]*)/g
+  let params = {}
+  let tokens
+  while (tokens = re.exec(qs)) {
+    params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2])
+  }
+  return params
+})()
+
+function sortTable(tbl, n) {
+  var rows, i, x, y, shouldSwitch
+  var switchCount = 0
+  var switching = true
+  var dir = "asc"
+
+  while (switching) {
+    switching = false
+    rows = tbl.getElementsByTagName("tr")
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false
+      x = rows[i].getElementsByTagName("td")[n].innerHTML
+      x = isNaN(x) ? x.toLowerCase() : parseFloat(x)
+      y = rows[i + 1].getElementsByTagName("td")[n].innerHTML
+      y = isNaN(y) ? y.toLowerCase() : parseFloat(y)
+
+      if (dir === "asc") {
+        if (x > y) {
+          shouldSwitch= true
+          break
+        }
+      } else if (dir === "desc") {
+        if (x < y) {
+          shouldSwitch= true
+          break
+        }
+      }
+    }
+
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+      switching = true
+      switchCount ++
+    } else {
+      if (switchCount === 0 && dir === "asc") {
+        dir = "desc"
+        switching = true
+      }
+    }
+  }
+}
+// </region>
+
+/*
+██████  ██    ██ ██ ██      ██████  ███████ ██████  ███████
+██   ██ ██    ██ ██ ██      ██   ██ ██      ██   ██ ██
+██████  ██    ██ ██ ██      ██   ██ █████   ██████  ███████
+██   ██ ██    ██ ██ ██      ██   ██ ██      ██   ██      ██
+██████   ██████  ██ ███████ ██████  ███████ ██   ██ ███████
+*/
+// <region> Builders
+function buildHeader (inst) {
+  for (a of inst.links) {
+    $('.flex-header').append(`<a href="${a.link}">${a.display}</a>`)
+  }
+}
+
+function buildContents (div) {
+  $('#main-content').get(0).innerHTML += '<div></div>'
+  let par = $('#main-content div').get(-1)
+
+  for (prt of div) {
+    let add = ''
+
+    switch (prt.proton) {
+      case 'textbox':
+        add = `<p>${prt.text.join('</p><p>')}</p></div>`
+        break
+      case 'list':
+        if (prt.group !== undefined) {
+          let groups = {}
+          add = `<table>`
+
+          for (li of prt.items) {
+            if (groups[li[prt.group]] === undefined) {
+              groups[li[prt.group]] = []
+            }
+            groups[li[prt.group]].push(li)
+          }
+
+          for (var g in groups) {
+            if (groups.hasOwnProperty(g)) {
+              add += `<tr><td colspan="${Object.keys(prt.header).length - 1}"><h3>${g}</h3></td></tr>`
+              add += `<tr><th>${prt.header.slice(prt.group + 1).join('</th><th>')}</th></tr>`
+
+              for (li of groups[g]) {
+                add += `<tr><td>${li.slice(prt.group + 1).join('</td><td>')}</td></tr>`
+              }
+            }
+          }
+          add += `</table>`
+
+        } else {
+          add = `<table><thead><tr><th>${prt.header.join('</th><th>')}</th></tr></thead><tbody>`
+          for (li of prt.items) {
+            add += `<tr><td>${li.join('</td><td>')}</td></tr>`
+          }
+          add += `</tbody></table>`
+        }
+
+        break
+    }
+    par.innerHTML += `
+      <div class="proton-${prt.proton}">
+        <h2 class="text-title">${prt.title}</h2>
+        <hr>
+        ${add}
+      </div>
+    `
+  }
+}
+// </region>
+
+/*
+██       ██████   █████  ██████
+██      ██    ██ ██   ██ ██   ██
+██      ██    ██ ███████ ██   ██
+██      ██    ██ ██   ██ ██   ██
+███████  ██████  ██   ██ ██████
+*/
+// <region> Load
+$(function () {
+  window.__settings = remote.getGlobal('__settings')
+  window.subsites = remote.getGlobal('subsites')
+  changeSite('home')
+})
+
+function changeSite (name) {
+  window.__site = name
+  let src = path.join(remote.app.getPath('userData'), 'sites', `${window.__site}.js`)
+  if (window.__site === 'home') {src = path.join(__dirname, 'home.js')}
+
+  $.getScript(src, () => {
+    siteEmpty()
+    siteLoad()
+  })
+}
+function siteEmpty () {
+  $('.flex-header').html('<h1 id="site-title"></h1>')
+  $('#main-content').html('')
+}
+function siteLoad () {
+  document.title = `GameDay - ${PAGE.title}`
+  $('#site-title').html(PAGE.title)
+
+  buildHeader(PAGE.header)
+  for (div of PAGE.contents) {
+    buildContents(div)
+  }
+  protonsLoad()
+}
+
+function protonsLoad () {
+  var table = $('.proton-list table')
+  $('.proton-list span.sortable').each(function () {
+    $(this).click(function () {
+      console.log($(this).parents('table').get(0), $(this).parent().index())
+      sortTable($(this).parents('table').get(0), $(this).parent().index())
+      this.innerHTML = this.innerHTML === '▼' ? '▲' : '▼'
+    })
+  })
+}
+// </region>
